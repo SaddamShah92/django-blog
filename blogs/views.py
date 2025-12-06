58683788c0dc8271a97478f97a5416e42e0366f8
Syed Saddam Shah <saddamshah415@gmail.com>
2025-12-05 19:21:45 -0500

    implement context_processors and base.html

--- blogs/views.py
+++ blogs/views.py
@@ -5,7 +5,8 @@ from .models import Blog, Category
 def posts_by_category(request, category_id):
     # Fetch the posts that belongs to the category with the id category_id
     posts = Blog.objects.filter(status = 'Published', category = category_id)
-    category = get_object_or_404(Category, pk = category_id)
+    category = get_object_or_404(Category, pk = category_id) # we can also use try/except when we want to do some custom action if the category does not exists
+
     context = {
         'posts' : posts,
         'category' : category,
