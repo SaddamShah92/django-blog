696794c1d44d4caf24c7af446d270fc0124cc6b0
Syed Saddam Shah <saddamshah415@gmail.com>
2025-12-08 20:12:23 -0500

    blogs and seach feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from assignments.models import About
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False, status = 'Published').order_by('updated_at')
+
+    # Fetch about us
+
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'featured_posts' : featured_posts,
-        'posts' : posts
+        'posts' : posts,
+        'about' : about,
     }
    
     return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Blog, Category
+from django.db.models import Q
+
 
 def posts_by_category(request, category_id):
     # Fetch the posts that belongs to the category with the id category_id
@@ -20,3 +22,19 @@ def blogs(request, slug):
         'single_blog' : single_blog,
     }
     return render(request, 'blogs.html' , context)
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    # print('keyword==>' , keyword)
+
+    blogs = Blog.objects.filter(Q(title__icontains = keyword) | Q(short_description__icontains = keyword) | Q(blog_body__icontains = keyword), status = 'Published')
+
+    context = {
+
+        'blogs' : blogs,
+        'keyword' : keyword,
+    }
+
+
+    return render(request, 'search.html', context)
