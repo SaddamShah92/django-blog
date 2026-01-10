c6f357c32a96b0a2ecc5564c26b0857f97b00cec
Syed Saddam Shah <saddamshah415@gmail.com>
2026-01-09 20:55:49 -0500

    Implement custom 404.html and polish dashboard UI

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ogf8t4i!($o8w+*(fuc12mo&zsxk$xzw-^9e=)0zf7d%bq^h('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['saddamshah92.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['saddamshah92.pythonanywhere.com', '127.0.0.1', 'localhost', 'localhost:8000']
 
 
 # Application definition
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'blog_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
