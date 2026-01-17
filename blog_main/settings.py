c0edd6f48e53f295e1a59cefbe80ebd35b79a250
Syed Saddam Shah <saddamshah415@gmail.com>
2026-01-16 19:16:26 -0500

    added allowed hosts info

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1ogf8t4i!($o8w+*(fuc12mo&zsxk$xzw-^9e=)0zf7d%bq^h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['saddamshah92.pythonanywhere.com', '127.0.0.1', 'localhost', 'localhost:8000']
+ALLOWED_HOSTS = ['saddamshah92.pythonanywhere.com', '127.0.0.1', 'localhost', 'localhost:8000', 'django-blog-trfi.onrender.com', 'saddamshah.com']
 
 
 # Application definition
